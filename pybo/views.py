55f83213abb2608c95b533054a1245a3be5267ef
서준아인아빠 <checco17naver@gmail.com>
2021-05-31 01:45:10 +0000

    subRid 라는 파라메타 추가하여 detail/리소스 번호 받을수 있도록 수정진행.

--- pybo/views.py
+++ pybo/views.py
@@ -12,6 +12,7 @@ def index(request):
 
 def item_price_get(request):
     redid = request.GET['redid']
+    subRid = request.GET['subRid']
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -24,192 +25,197 @@ def item_price_get(request):
     
     time.sleep(1)
     url_default = 'https://www.redprinting.co.kr/ko/product/item/'+redid[0:2]+'/'+redid
-
-    if redid == "TPBCDFT":
-        url = url_default+'/detail/26243'
-    elif redid == "TPBCCPN":
-        url = url_default+'/detail/179004'
-    elif redid == "TPWBDFT":
-        url = url_default+'/detail/36982'
-    elif redid == "TPPOCHR":
-        url = url_default+'/detail/234897'
-    elif redid == "FBDCMOS":
-        url = url_default+'/detail/194064'
-    elif redid == "FBDCGLS":
-        url = url_default+'/detail/195780'
-    elif redid == "GSWTANT":
-        url = url_default+'/detail/995770'
-    elif redid == "BTMGPNS":
-        url = url_default+'/detail/505239'
-    elif redid == "TPTKBND":
-        url = url_default+'/detail/669995'
-    elif redid == "TPWTDFT":
-        url = url_default+'/detail/32374'
-    elif redid == "GSTRTAG":
-        url = url_default+'/detail/30947'
-    elif redid == "GSFGMIN":
-        url = url_default+'/detail/246348'
-    elif redid == "TPLFSET":
-        url = url_default+'/detail/256623'
-    elif redid == "PRBNDGN":
-        url = url_default+'/detail/234518'
-    elif redid == "PHSTPAN":
-        url = url_default+'/detail/173824'
-    elif redid == "PHSTNOP":
-        url = url_default+'/detail/228437'
-    elif redid == "PHSTSQP":
-        url = url_default+'/detail/167769'
-    elif redid == "TPCASET":
-        url = url_default+'/detail/253311'
-    elif redid == "TPDCPST":
-        url = url_default+'/detail/40374'
-    elif redid == "GSNTBND":
-        url = url_default+'/detail/938675'
-    elif redid == "GSNTLTR":
-        url = url_default+'/detail/942060'
-    elif redid == "GSPNCLP":
-        url = url_default+'/detail/1192362'
-    elif redid == "GSPNFLT":
-        url = url_default+'/detail/1276112'
-    elif redid == "TPPOAWD":
-        url = url_default+'/detail/29439'
-    elif redid == "TPRNBND":
-        url = url_default+'/detail/286280'
-    elif redid == "GSCLMGN":
-        url = url_default+'/detail/223345'
-    elif redid == "GSMEMGN":
-        url = url_default+'/detail/341125'
-    elif redid == "GSSMSTP":
-        url = url_default+'/detail/935948'
-    elif redid == "GSSMTMP":
-        url = url_default+'/detail/951107'
-    elif redid == "GSSMRUB":
-        url = url_default+'/detail/F1'
-    elif redid == "GSSMWOD":
-        url = url_default+'/detail/994421'
-    elif redid == "GSSMWDR":
-        url = url_default+'/detail/1155399'
-    elif redid == "GSTTCRK":
-        url = url_default+'/detail/256120'
-    elif redid == "GSTTREZ":
-        url = url_default+'/detail/283796'
-    elif redid == "FBTWSWT":
-        url = url_default+'/detail'
-    elif redid == "FBPCLKB":
-        url = url_default+'/detail/676886'
-    elif redid == "AIECMNP":
-        url = url_default+'/detail'
-    elif redid == "FBPODFT":
-        url = url_default+'/detail/406004'
-    elif redid == "FBCLTSH":
-        url = url_default+'/detail/525985'
-    elif redid == "FBCLBSS":
-        url = url_default+'/detail'
-    elif redid == "FBDCTWL":
-        url = url_default+'/detail/193980'
-    elif redid == "GSCAPAS":
-        url = url_default+'/detail/236824'
-    elif redid == "GSFBSTK":
-        url = url_default+'/detail/244371'
-    elif redid == "GSLPSTK":
-        url = url_default+'/detail/308317'
-    elif redid == "GSLPPRT":
-        url = url_default+'/detail/362191'
-    elif redid == "GSLPCVR":
-        url = url_default+'/detail/314600'
-    elif redid == "GSWLMIN":
-        url = url_default+'/detail/276827'
-    elif redid == "GSWLCAD":
-        url = url_default+'/detail/317114'
-    elif redid == "GSWLNEC":
-        url = url_default+'/detail/317525'
-    elif redid == "GSCAPHN":
-        url = url_default+'/detail/1116838'
-    elif redid == "TPHPFLM":
-        url = url_default+'/detail/357621'
-    elif redid == "GSCAPOD":
-        url = url_default+'/detail/360425'
-    elif redid == "GSCABUZ":
-        url = url_default+'/detail/983356'
-    elif redid == "GSMODFT":
-        url = url_default+'/detail/127811'
-    elif redid == "GSCKDFT":
-        url = url_default+'/detail/655754'
-    elif redid == "ACTPKEY":
-        url = url_default+'/detail/294141'
-    elif redid == "GSSTKEY":
-        url = url_default+'/detail/310362'
-    elif redid == "GSCATIN":
-        url = url_default+'/detail/312698'
-    elif redid == "GSSBMTL":
-        url = url_default+'/detail/46'
-    elif redid == "GSCBSTK":
-        url = url_default+'/detail/719969'
-    elif redid == "GSCAMSK":
-        url = url_default+'/detail/1106659'
-    elif redid == "PHFRACR":
-        url = url_default+'/detail/271588'
-    elif redid == "ACNTHAP":
-        url = url_default+'/detail/374855'
-    elif redid == "BNSTDGN":
-        url = url_default+'/detail/234319'
-    elif redid == "AICNDGN":
-        url = url_default+'/detail/984727'
-    elif redid == "PHPRDFT":
-        url = url_default+'/detail/223791'
-    elif redid == "PHPKDFT":
-        url = url_default+'/detail/308784'
-    elif redid == "TPPHSET":
-        url = url_default+'/detail/296202'
-    elif redid == "PHBKBKS":
-        url = url_default+'/detail/242508?sel_gbn=8x6'
-    elif redid == "PHBKSMP":
-        url = url_default+'/detail/242484?sel_gbn=8x6'
-    elif redid == "PHBKPRM":
-        url = url_default+'/detail/242496?sel_gbn=8x6'
-    elif redid == "PHPLEDT":
-        url = url_default+'/detail/697364'
-    elif redid == "GSPZPHO":
-        url = url_default+'/detail/197964'
-    elif redid == "PHBKSTA":
-        url = url_default+'/detail/262083'
-    elif redid == "TPCLWLB":
-        url = url_default+'/detail/760777'
-    elif redid == "TPCLSTD":
-        url = url_default+'/detail/600877'
-    elif redid == "TPCLHOL":
-        url = url_default+'/detail/600926'
-    elif redid == "TPCLWAL":
-        url = url_default+'/detail/600921'
-    elif redid == "PHFRCWC":
-        url = url_default+'/detail/27065'
-    elif redid == "PHFRHAN":
-        url = url_default+'/detail/27095'
-    elif redid == "PHFRASH":
-        url = url_default+'/detail/27274'
-    elif redid == "PHFRSVL":
-        url = url_default+'/detail/27331'
-    elif redid == "PHFRFIR":
-        url = url_default+'/detail/26732'
-    elif redid == "PHFRMUL":
-        url = url_default+'/detail/27118'
-    elif redid == "PHFRALU":
-        url = url_default+'/detail/343666'
-    elif redid == "PHFRWOD":
-        url = url_default+'/detail/355121'
-    elif redid == "PHFRPAP":
-        url = url_default+'/detail/272793'
-    elif redid == "GSCRCUB":
-        url = url_default+'/detail/359901'
-    elif redid == "GSGLPAN":
-        url = url_default+'/detail/181895'
+    if subRid != "":
+        url = url_default+subRid
     else:
-        url = url_default
+        if redid == "TPBCDFT":
+            url = url_default+'/detail/26243'
+        elif redid == "TPBCCPN":
+            url = url_default+'/detail/179004'
+        elif redid == "TPWBDFT":
+            url = url_default+'/detail/36982'
+        elif redid == "TPPOCHR":
+            url = url_default+'/detail/234897'
+        elif redid == "FBDCMOS":
+            url = url_default+'/detail/194064'
+        elif redid == "FBDCGLS":
+            url = url_default+'/detail/195780'
+        elif redid == "GSWTANT":
+            url = url_default+'/detail/995770'
+        elif redid == "BTMGPNS":
+            url = url_default+'/detail/505239'
+        elif redid == "TPTKBND":
+            url = url_default+'/detail/669995'
+        elif redid == "TPWTDFT":
+            url = url_default+'/detail/32374'
+        elif redid == "GSTRTAG":
+            url = url_default+'/detail/30947'
+        elif redid == "GSFGMIN":
+            url = url_default+'/detail/246348'
+        elif redid == "TPLFSET":
+            url = url_default+'/detail/256623'
+        elif redid == "PRBNDGN":
+            url = url_default+'/detail/234518'
+        elif redid == "PHSTPAN":
+            url = url_default+'/detail/173824'
+        elif redid == "PHSTNOP":
+            url = url_default+'/detail/228437'
+        elif redid == "PHSTSQP":
+            url = url_default+'/detail/167769'
+        elif redid == "TPCASET":
+            url = url_default+'/detail/253311'
+        elif redid == "TPDCPST":
+            url = url_default+'/detail/40374'
+        elif redid == "GSNTBND":
+            url = url_default+'/detail/938675'
+        elif redid == "GSNTLTR":
+            url = url_default+'/detail/942060'
+        elif redid == "GSPNCLP":
+            url = url_default+'/detail/1192362'
+        elif redid == "GSPNFLT":
+            url = url_default+'/detail/1276112'
+        elif redid == "TPPOAWD":
+            url = url_default+'/detail/29439'
+        elif redid == "TPRNBND":
+            url = url_default+'/detail/286280'
+        elif redid == "GSCLMGN":
+            url = url_default+'/detail/223345'
+        elif redid == "GSMEMGN":
+            url = url_default+'/detail/341125'
+        elif redid == "GSSMSTP":
+            url = url_default+'/detail/935948'
+        elif redid == "GSSMTMP":
+            url = url_default+'/detail/951107'
+        elif redid == "GSSMRUB":
+            url = url_default+'/detail/F1'
+        elif redid == "GSSMWOD":
+            url = url_default+'/detail/994421'
+        elif redid == "GSSMWDR":
+            url = url_default+'/detail/1155399'
+        elif redid == "GSTTCRK":
+            url = url_default+'/detail/256120'
+        elif redid == "GSTTREZ":
+            url = url_default+'/detail/283796'
+        elif redid == "FBTWSWT":
+            url = url_default+'/detail'
+        elif redid == "FBPCLKB":
+            url = url_default+'/detail/676886'
+        elif redid == "AIECMNP":
+            url = url_default+'/detail'
+        elif redid == "FBPODFT":
+            url = url_default+'/detail/406004'
+        elif redid == "FBCLTSH":
+            url = url_default+'/detail/525985'
+        elif redid == "FBCLBSS":
+            url = url_default+'/detail'
+        elif redid == "FBDCTWL":
+            url = url_default+'/detail/193980'
+        elif redid == "GSCAPAS":
+            url = url_default+'/detail/236824'
+        elif redid == "GSFBSTK":
+            url = url_default+'/detail/244371'
+        elif redid == "GSLPSTK":
+            url = url_default+'/detail/308317'
+        elif redid == "GSLPPRT":
+            url = url_default+'/detail/362191'
+        elif redid == "GSLPCVR":
+            url = url_default+'/detail/314600'
+        elif redid == "GSWLMIN":
+            url = url_default+'/detail/276827'
+        elif redid == "GSWLCAD":
+            url = url_default+'/detail/317114'
+        elif redid == "GSWLNEC":
+            url = url_default+'/detail/317525'
+        elif redid == "GSCAPHN":
+            url = url_default+'/detail/1116838'
+        elif redid == "TPHPFLM":
+            url = url_default+'/detail/357621'
+        elif redid == "GSCAPOD":
+            url = url_default+'/detail/360425'
+        elif redid == "GSCABUZ":
+            url = url_default+'/detail/983356'
+        elif redid == "GSMODFT":
+            url = url_default+'/detail/127811'
+        elif redid == "GSCKDFT":
+            url = url_default+'/detail/655754'
+        elif redid == "ACTPKEY":
+            url = url_default+'/detail/294141'
+        elif redid == "GSSTKEY":
+            url = url_default+'/detail/310362'
+        elif redid == "GSCATIN":
+            url = url_default+'/detail/312698'
+        elif redid == "GSSBMTL":
+            url = url_default+'/detail/46'
+        elif redid == "GSCBSTK":
+            url = url_default+'/detail/719969'
+        elif redid == "GSCAMSK":
+            url = url_default+'/detail/1106659'
+        elif redid == "PHFRACR":
+            url = url_default+'/detail/271588'
+        elif redid == "ACNTHAP":
+            url = url_default+'/detail/374855'
+        elif redid == "BNSTDGN":
+            url = url_default+'/detail/234319'
+        elif redid == "AICNDGN":
+            url = url_default+'/detail/984727'
+        elif redid == "PHPRDFT":
+            url = url_default+'/detail/223791'
+        elif redid == "PHPKDFT":
+            url = url_default+'/detail/308784'
+        elif redid == "TPPHSET":
+            url = url_default+'/detail/296202'
+        elif redid == "PHBKBKS":
+            url = url_default+'/detail/242508?sel_gbn=8x6'
+        elif redid == "PHBKSMP":
+            url = url_default+'/detail/242484?sel_gbn=8x6'
+        elif redid == "PHBKPRM":
+            url = url_default+'/detail/242496?sel_gbn=8x6'
+        elif redid == "PHPLEDT":
+            url = url_default+'/detail/697364'
+        elif redid == "GSPZPHO":
+            url = url_default+'/detail/197964'
+        elif redid == "PHBKSTA":
+            url = url_default+'/detail/262083'
+        elif redid == "TPCLWLB":
+            url = url_default+'/detail/760777'
+        elif redid == "TPCLSTD":
+            url = url_default+'/detail/600877'
+        elif redid == "TPCLHOL":
+            url = url_default+'/detail/600926'
+        elif redid == "TPCLWAL":
+            url = url_default+'/detail/600921'
+        elif redid == "PHFRCWC":
+            url = url_default+'/detail/27065'
+        elif redid == "PHFRHAN":
+            url = url_default+'/detail/27095'
+        elif redid == "PHFRASH":
+            url = url_default+'/detail/27274'
+        elif redid == "PHFRSVL":
+            url = url_default+'/detail/27331'
+        elif redid == "PHFRFIR":
+            url = url_default+'/detail/26732'
+        elif redid == "PHFRMUL":
+            url = url_default+'/detail/27118'
+        elif redid == "PHFRALU":
+            url = url_default+'/detail/343666'
+        elif redid == "PHFRWOD":
+            url = url_default+'/detail/355121'
+        elif redid == "PHFRPAP":
+            url = url_default+'/detail/272793'
+        elif redid == "GSCRCUB":
+            url = url_default+'/detail/359901'
+        elif redid == "GSGLPAN":
+            url = url_default+'/detail/181895'
+        else:
+            url = url_default
     driver.get(url)
     if redid in item_list:
-        price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
-        if price_text == "":
-            price_text = driver.find_element_by_name('total_price').get_attribute("value")
+        t_price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
+        if t_price_text == "":
+            t_price_text = driver.find_element_by_name('total_price').get_attribute("value")
+
+        s_price_text = driver.find_element_by_xpath("//div[@id='PRICE_DIS2']").text
+        v_price_text = driver.find_element_by_xpath("//div[@id='PRICE_SUB_DIS2']").text
         paper_text = ""
         paper_sub_text = ""
         paper_dosu = ""
@@ -220,9 +226,12 @@ def item_price_get(request):
         item_count = ""
         item_case = ""
     else:
-        price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
-        if price_text == "":
-            price_text = driver.find_element_by_name('total_price').get_attribute("value")
+        t_price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
+        if t_price_text == "":
+            t_price_text = driver.find_element_by_name('total_price').get_attribute("value")
+
+        s_price_text = driver.find_element_by_xpath("//div[@id='PRICE_DIS2']").text
+        v_price_text = driver.find_element_by_xpath("//div[@id='PRICE_SUB_DIS2']").text
 
         paper_text = driver.find_element_by_id('paperSelectBoxItText').text
         # paper_sub_text = driver.find_element_by_id('paper_sub_selectSelectBoxItText').text
@@ -241,7 +250,9 @@ def item_price_get(request):
 
     driver.quit()
     data = {
-        "price" : price_text,                    # 금액
+        "total_price" : t_price_text,                    # 총금액
+        "sum_price" : s_price_text,                    # 합계
+        "vat_price" : v_price_text,                    # 부가세
         "paper_name" : paper_text,               # 용지명
         # "paper_sub_name" : paper_sub_text,       # 용지g수
         # "paper_dosu" : paper_dosu,               # 인쇄도수
