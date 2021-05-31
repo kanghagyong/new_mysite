20da8c322a1a5da989c10089a0bc51c69fe0e179
서준아인아빠 <checco17naver@gmail.com>
2021-05-31 09:22:20 +0000

    try except문을 통해서 오류 발생시 status error 을 넘기도록 코드 수정.

--- pybo/views.py
+++ pybo/views.py
@@ -12,6 +12,7 @@ def index(request):
 
 def item_price_get(request):
     redid = request.GET['redid']
+    # subRid = request.GET['subRid']
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -24,62 +25,83 @@ def item_price_get(request):
     
     time.sleep(1)
     url = 'https://www.redprinting.co.kr/ko/product/item/'+redid[0:2]+'/'+redid
+    # if subRid != "":
+    #     url = url + "/detail/" + subRid
 
     driver.get(url)
-    if redid in item_list:
-        t_price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
-        if t_price_text == "":
-            t_price_text = driver.find_element_by_name('total_price').get_attribute("value")
+    try:
+        if redid in item_list:
+            t_price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
+            if t_price_text == "":
+                t_price_text = driver.find_element_by_name('total_price').get_attribute("value")
 
-        s_price_text = driver.find_element_by_xpath("//div[@id='PRICE_DIS2']").text
-        v_price_text = driver.find_element_by_xpath("//div[@id='PRICE_SUB_DIS2']").text
-        paper_text = ""
-        paper_sub_text = ""
-        paper_dosu = ""
-        size_c_x = ""
-        size_c_y = ""
-        size_w_x = ""
-        size_w_y = ""
-        item_count = ""
-        item_case = ""
-    else:
-        t_price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
-        if t_price_text == "":
-            t_price_text = driver.find_element_by_name('total_price').get_attribute("value")
+            s_price_text = driver.find_element_by_xpath("//div[@id='PRICE_DIS2']").text
+            v_price_text = driver.find_element_by_xpath("//div[@id='PRICE_SUB_DIS2']").text
+            paper_text = ""
+            paper_sub_text = ""
+            paper_dosu = ""
+            size_c_x = ""
+            size_c_y = ""
+            size_w_x = ""
+            size_w_y = ""
+            item_count = ""
+            item_case = ""
+        else:
+            t_price_text = driver.find_element_by_xpath("//div[@id='TOTAL_PRICE']").text
+            if t_price_text == "":
+                t_price_text = driver.find_element_by_name('total_price').get_attribute("value")
 
-        s_price_text = driver.find_element_by_xpath("//div[@id='PRICE_DIS2']").text
-        v_price_text = driver.find_element_by_xpath("//div[@id='PRICE_SUB_DIS2']").text
+            s_price_text = driver.find_element_by_xpath("//div[@id='PRICE_DIS2']").text
+            v_price_text = driver.find_element_by_xpath("//div[@id='PRICE_SUB_DIS2']").text
 
-        paper_text = driver.find_element_by_id('paperSelectBoxItText').text
-        # paper_sub_text = driver.find_element_by_id('paper_sub_selectSelectBoxItText').text
-        # paper_dosu = driver.find_element_by_id('soduSelectBoxItText').text
-        size_c_x = driver.find_element_by_name('CUT_WDT').get_attribute("value")
-        size_c_y = driver.find_element_by_name('CUT_HGH').get_attribute("value")
-        size_w_x = driver.find_element_by_name('WRK_WDT').get_attribute("value")
-        size_w_y = driver.find_element_by_name('WRK_HGH').get_attribute("value")
-        item_count = driver.find_element_by_name('number1').get_attribute("value")
-        if item_count == "":
-            item_count = driver.find_element_by_id('number1_selSelectBoxItText').text
+            paper_text = driver.find_element_by_id('paperSelectBoxItText').text
+            # paper_sub_text = driver.find_element_by_id('paper_sub_selectSelectBoxItText').text
+            # paper_dosu = driver.find_element_by_id('soduSelectBoxItText').text
+            size_c_x = driver.find_element_by_name('CUT_WDT').get_attribute("value")
+            size_c_y = driver.find_element_by_name('CUT_HGH').get_attribute("value")
+            size_w_x = driver.find_element_by_name('WRK_WDT').get_attribute("value")
+            size_w_y = driver.find_element_by_name('WRK_HGH').get_attribute("value")
+            item_count = driver.find_element_by_name('number1').get_attribute("value")
+            if item_count == "":
+                item_count = driver.find_element_by_id('number1_selSelectBoxItText').text
 
-        item_case = driver.find_element_by_name('number2').get_attribute("value")
-        if item_case == "":
-            item_case = driver.find_element_by_id('number2_selSelectBoxItText').text
-
-    driver.quit()
-    data = {
-        "total_price" : t_price_text,                    # 총금액
-        "sum_price" : s_price_text,                    # 합계
-        "vat_price" : v_price_text,                    # 부가세
-        "paper_name" : paper_text,               # 용지명
-        # "paper_sub_name" : paper_sub_text,       # 용지g수
-        # "paper_dosu" : paper_dosu,               # 인쇄도수
-        "size_cut_X" : size_c_x,                 # 재단사이즈x
-        "size_cut_Y" : size_c_y,                 # 재단사이즈y
-        "size_work_X" : size_w_x,                # 작업사이즈x
-        "size_work_Y" : size_w_y,                # 작업사이즈y
-        "item_count" : item_count,               # 수량
-        "item_case" : item_case                  # 건수
-    }
+            item_case = driver.find_element_by_name('number2').get_attribute("value")
+            if item_case == "":
+                item_case = driver.find_element_by_id('number2_selSelectBoxItText').text
+        driver.quit()
+        data = {
+            "status" : "ok",                    # 상태
+            "total_price" : t_price_text,                    # 총금액
+            "sum_price" : s_price_text,                    # 합계
+            "vat_price" : v_price_text,                    # 부가세
+            "paper_name" : paper_text,               # 용지명
+            # "paper_sub_name" : paper_sub_text,       # 용지g수
+            # "paper_dosu" : paper_dosu,               # 인쇄도수
+            "size_cut_X" : size_c_x,                 # 재단사이즈x
+            "size_cut_Y" : size_c_y,                 # 재단사이즈y
+            "size_work_X" : size_w_x,                # 작업사이즈x
+            "size_work_Y" : size_w_y,                # 작업사이즈y
+            "item_count" : item_count,               # 수량
+            "item_case" : item_case                  # 건수
+        }
+    except:
+        driver.quit()
+        data = {
+            "status" : "error",                    # 상태
+            "total_price" : 0,                    # 총금액
+            "sum_price" : 0,                    # 합계
+            "vat_price" : 0,                    # 부가세
+            "paper_name" : 0,               # 용지명
+            # "paper_sub_name" : paper_sub_text,       # 용지g수
+            # "paper_dosu" : paper_dosu,               # 인쇄도수
+            "size_cut_X" : 0,                 # 재단사이즈x
+            "size_cut_Y" : 0,                 # 재단사이즈y
+            "size_work_X" : 0,                # 작업사이즈x
+            "size_work_Y" : 0,                # 작업사이즈y
+            "item_count" : 0,               # 수량
+            "item_case" : 0                  # 건수
+        }
+    
     return JsonResponse(data)
 
 # ---------------------------------------------------------------------------- #
